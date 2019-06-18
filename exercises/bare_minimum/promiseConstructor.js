c84a472dbd6b8d78f155826477af7826132dc40d
kbsjsn <shin.kirk@gmail.com>
2019-06-17 19:41:01 -0700

    Completes promiseConstructor

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -7,16 +7,12 @@
 var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
+var nodeStyle = require('./callbackReview.js');
 
-// This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
-};
+var pluckFirstLineFromFileAsync = Promise.promisify(nodeStyle.pluckFirstLineFromFile);
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCodeAsync = function(url) {
-  // TODO
-};
+var getStatusCodeAsync = Promise.promisify(nodeStyle.getStatusCode);
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {

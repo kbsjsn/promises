fb5ccbdeb33af8f75519c96b0ac63f1d24d58e28
kbsjsn <shin.kirk@gmail.com>
2019-06-18 10:36:21 -0700

    Completes promisification

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -26,6 +26,15 @@ var pluckFirstLineFromFileAsync = function(filePath) {
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return new Promise ((resolve, reject) => {
+    request(url, (err, status) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(status.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
